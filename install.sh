6f44a3a90b9b308cf7013b358259981c7d7c972a
root <root@raspberrypi.(none)>
2014-11-07 01:13:40 +0100

    Rearrangement

--- install.sh
+++ install.sh
@@ -5,7 +5,16 @@ declare -A src
 src['/usr/src/aircrack-ng']="svn co http://svn.aircrack-ng.org/trunk/ /usr/src/aircrack-ng && cd /usr/src/aircrack-ng && make install"
 src['/usr/src/wiringPi']="git clone git://git.drogon.net/wiringPi /usr/src/wiringPi && cd /usr/src/wiringPi && ./build"
 src['/usr/src/pcd8544']='git clone https://github.com/XavierBerger/pcd8544.git /usr/src/pcd8544'
-src['/usr/src/wifite']="git clone git@github.com:Korsani/wifite.git /usr/src/wifite && mkdir -p $HOME/bin && ln -f -s /usr/src/wifite/wifite.py $HOME/bin/wifite.py"
+src['/usr/src/wifite']="git clone https://github.com/Korsani/wifite.git /usr/src/wifite && mkdir -p $HOME/bin && ln -f -s /usr/src/wifite/wifite.py $HOME/bin/wifite.py"
+egrep '^-' $PACKAGES_FILE | sed -e 's/^-//' | xargs apt-get -q -y purge 
+egrep '^\+' $PACKAGES_FILE | sed -e 's/^\+//' | xargs apt-get -y install 
+while [ -n "$(deborphan)" ]
+do
+	apt-get -y purge $(deborphan)
+done
+apt-get -y autoremove 
+dpkg -l | egrep '^rc' | awk '{print $2}' | xargs apt-get -y purge
+rpi-update
 for d in "${!src[@]}"
 do
 	if [ ! -d $d ]
@@ -13,9 +22,8 @@ do
 		eval ${src[$d]}
 	fi
 done
-egrep '^-' $PACKAGES_FILE | sed -e 's/^-//' | xargs apt-get -y purge 
-egrep '^\+' $PACKAGES_FILE | sed -e 's/^\+//' | xargs apt-get -y install 
-apt-get -y purge $(deborphan)
-apt-get -y purge $(deborphan)
-apt-get -y purge $(deborphan)
-apt-get -y autoremove 
+if ! $(grep -q 'tmpfs /tmp tmpfs' /proc/mounts)
+then
+	echo 'tmpfs           /tmp            tmpfs   defaults        0       0' >> /etc/fstab
+	mount /tmp
+fi
