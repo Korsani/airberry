cb35d83b4b8ad06ff28e881be731dc5597f46586
Gabriel Guillon <korsani@free.fr>
2014-11-18 11:56:31 +0100

    Adding colors

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+source libkoca.sh
+getColor _w white _e reset _p purple _r hired
 PACKAGES_FILE=packages
 WHAT="packages interfaces rpi-update sources fstab"
+do1="${_p}*${_e}"
+do2="${_p}**${_e}"
+do3="${_p}***${_e}"
 [ $(id -u) -ne 0 ] && echo "I have to be run as root" && exit 1
 [ $(uname -m) != "armv6l" ] && echo "Not on RPi" && exit 1
-echo "This WILL break things. Please press enter to continue"
+echo "${_r}!!!${_e} This ${_r}WILL${_e} break things. Please press enter to continue"
 read
 if  ! $(netstat -r | grep -q default) 
 then
@@ -16,16 +21,22 @@ src['/usr/src/wiringPi']="git clone git://git.drogon.net/wiringPi /usr/src/wirin
 src['/usr/src/pcd8544']='git clone https://github.com/XavierBerger/pcd8544.git /usr/src/pcd8544'
 src['/usr/src/wifite']="git clone https://github.com/Korsani/wifite.git /usr/src/wifite && mkdir -p $HOME/bin && ln -f -s /usr/src/wifite/wifite.py $HOME/bin/wifite.py"
 function packages() {
+	echo "$do1 Uninstalling packages"
 	egrep '^-' $PACKAGES_FILE | sed -e 's/^-//' | xargs apt-get -q -y purge 
+	echo "$do1 Installing packages"
 	egrep '^\+' $PACKAGES_FILE | sed -e 's/^\+//' | xargs apt-get -y install 
+	echo "$do1 Removing orphans"
 	while [ -n "$(deborphan)" ]
 	do
 		apt-get -y purge $(deborphan)
 	done
+	echo "$do1 Running autoremove"
 	apt-get -y autoremove 
+	echo "$do1 Purging conf files of uninstalled packages"
 	dpkg -l | egrep '^rc' | awk '{print $2}' | xargs apt-get -y purge
 }
 function interfaces() {
+	echo "$do1 Installing network interfaces"
 	cp -a etc_network_interfaces /etc/network/interfaces 
 }
 function sources() {
@@ -33,6 +44,7 @@ function sources() {
 	do
 		if [ ! -d $d ]
 		then
+			echo "$do1 Installing $(basename $d)"
 			eval ${src[$d]}
 		fi
 	done
@@ -40,19 +52,22 @@ function sources() {
 function fstab() {
 	if [ ! -e /etc/fstab.d/tmpfs ]
 	then
+		echo "$do1 Installing tmp as tmpfs"
 		echo 'tmpfs           /tmp            tmpfs   defaults        0       0' >> /etc/fstab.d/tmpfs
 		mount -t tmpfs tmpfs /tmp
 	fi
+	echo "$do1 Puting sync option to /"
 	sed -i.kocrack-root -e '/mmcblk0p2/c\/dev/mmcblk0p2  /               ext4    defaults,noatime,sync  0       1' /etc/fstab && mount -o remount /
+	echo "$do1 Puting sync option to /boot"
 	sed -i.kocrack-boot -e '/mmcblk0p1/c\/dev/mmcblk0p1  /boot           vfat    defaults,sync          0       2' /etc/fstab && mount -o remount /boot
 }
 if [ -n "$*" ]
 then
 	WHAT="$*"
 fi
-echo "*** Will run : $WHAT"
+echo "$do3 Will run : ${_w}$WHAT${_e}"
 for what in $WHAT
 do
-	echo "** Proceeding with $what"
+	echo "$do2 Proceeding with ${_w}$what${_e}"
 	$what
 done
