179961239e1bbcef335f87022cbf49ce701f820d
Gabriel Guillon <korsani@free.fr>
2014-11-20 01:12:26 +0100

    Adding comments

--- install.sh
+++ install.sh
@@ -1,20 +1,32 @@
 #!/usr/bin/env bash
+# Wonderful library
 source libkoca.sh
+# to get colored output
 getColor _w white _e reset _p purple _r hired
-PACKAGES_FILE=packages
-WHAT="packages interfaces rpi-update sources fstab"
 do1="${_p}*${_e}"
 do2="${_p}**${_e}"
 do3="${_p}***${_e}"
-[ $(id -u) -ne 0 ] && echo "I have to be run as root" && exit 1
-[ $(uname -m) != "armv6l" ] && echo "Not on RPi" && exit 1
+
+# what module to run, by dafault
+WHAT="packages interfaces rpi-update sources fstab fsck"
+# File listing package to add and remove
+PACKAGES_FILE=packages
+
+# Third part to install : their dir and how to build/install them
 declare -A src
 src['/usr/src/aircrack-ng']="svn co http://svn.aircrack-ng.org/trunk/ /usr/src/aircrack-ng && cd /usr/src/aircrack-ng && make install"
 src['/usr/src/wiringPi']="git clone git://git.drogon.net/wiringPi /usr/src/wiringPi && cd /usr/src/wiringPi && ./build"
 src['/usr/src/pcd8544']='git clone https://github.com/XavierBerger/pcd8544.git /usr/src/pcd8544'
 src['/usr/src/wifite']="git clone https://github.com/Korsani/wifite.git /usr/src/wifite && mkdir -p $HOME/bin && ln -f -s /usr/src/wifite/wifite.py $HOME/bin/wifite.py"
 src['/usr/src/dosfstools']="git clone http://daniel-baumann.ch/git/software/dosfstools.git /usr/src/dosfstools && cd /usr/src/dosfstools && make"
+
+# On exit, run this
 trap '_post' 0
+
+# Sanity check
+[ $(id -u) -ne 0 ] && echo "I have to be run as root" && exit 1
+[ $(uname -m) != "armv6l" ] && echo "Not on RPi" && exit 1
+
 function _pre(){
 	echo "$do3 executing _pre"
 	mount / -o remount,async
@@ -35,6 +47,10 @@ function _warn(){
 function help(){
 	egrep "^function [^_]" $0 | sed -e 's/function \(.*\)().*/\1/' | xargs
 }
+# (Un)install packages
+# Package file is like that :
+# +package_to_add
+# -package_to_remove 
 function packages() {
 	_warn
 	_check_default_route || return 1
@@ -57,6 +73,7 @@ function interfaces() {
 	echo "$do1 Installing network interfaces"
 	cp -a etc_network_interfaces /etc/network/interfaces 
 }
+# Third part software
 function sources() {
 	_check_default_route || return 1
 	if [ -n "$@" ]
@@ -74,6 +91,8 @@ function sources() {
 		fi
 	done
 }
+# Manipulate fstab : mount /tmp as tmpfs, and add sync options to / and /boot
+# And umount /boot
 function fstab() {
 	_warn
 	if [ ! -e "/etc/fstab.d/00-tmpfs.fstab" ]
@@ -87,7 +106,9 @@ function fstab() {
 	echo "$do1 Puting sync option to /boot"
 	sed -i.kocrack-boot -e '/mmcblk0p1/c\/dev/mmcblk0p1  /boot           vfat    defaults,noauto,sync          0       2' /etc/fstab
 	umount /boot 2>/dev/null
-}	
+}
+# fsck /boot
+# Need external dosfstools as the one shipped can't remove dirty flag (sic)
 function fsck(){
 	_warn
 	if  $(dmesg | grep -q 'FAT-fs (mmcblk0p1): Volume was not properly unmounted') 
@@ -99,11 +120,17 @@ function fsck(){
 		./fsck.fat -a -V /dev/mmcblk0p1
 	fi
 }
+# Update rpi
 function rpi-update(){
 	mount /boot
 	/usr/bin/rpi-update
 	umount /boot
 }
+#####
+# Run all of this
+#####
+
+# You can call me $0 part_to_run
 if [ -n "$*" ]
 then
 	WHAT="$*"
