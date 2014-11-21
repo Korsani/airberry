7504be720b0b7a0e62a01a7fbe04efda8129cf72
Gabriel Guillon <korsani@free.fr>
2014-11-21 01:02:22 +0100

    Use fifo to communicate

---
+++ check-fs-space.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+FS="$1"
+LOW_WATER_COMMAND="$2"
+[ -z "$2" ] && exit 1
+exec 5<>/var/run/kocrack
+while true
+do
+	space=$(df "$FS" | tail -1 | awk '{print $4}')
+	if [ "$space" -le $LOW_WATER_SPACE ]
+	then
+		echo "$(date +%s):$(basename "$0"):FS_FULL:Filesystem $FS full : ${space}k free" >&5
+	fi
+	sleep 10
+done
