da39e3790e8dda35172c1cc46a426a4a63b255b5
Gabriel Guillon <korsani@free.fr>
2014-11-30 21:21:00 +0100

    Cosmetic

--- check-fs-space.sh
+++ check-fs-space.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+eval "$(bash $(dirname "$0")/libkoca.sh koca_lockMe)"
+koca_lockMe /tmp/check-fs-space 0
 FS="$1"
 LOW_WATER_SPACE=500
+SOCKET=/var/run/kocrack
 [ -z "$FS" ] && exit 1
-exec 5<>/var/run/kocrack
+[ ! -p "$SOCKET" ] && exit 1
+exec 5<>$SOCKET
 while true
 do
 	space=$(df "$FS" | tail -1 | awk '{print $4}')
