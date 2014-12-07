6b74cfb15495e213e00e975906181e3807dca03b
Gabriel Guillon <korsani@free.fr>
2014-12-07 23:50:43 +0100

    Reindent

--- lcd.py
+++ lcd.py
@@ -4,12 +4,12 @@ import pcd8544.lcd as lcd
 import sys
 
 if __name__ == "__main__":
-	lcd.init()
-	lcd.set_contrast(190)
-	if sys.argv[1] == 'text':
-		lcd.locate(int(sys.argv[2]),int(sys.argv[3]))
-		lcd.text(sys.argv[4])
-	if sys.argv[1] == 'backlight':
-		lcd.backlight(int(sys.argv[2]))
-	if sys.argv[1] == 'cls':
-		lcd.cls()
+    lcd.init()
+    lcd.set_contrast(180)
+    if sys.argv[1] == 'text':
+        lcd.locate(int(sys.argv[2]),int(sys.argv[3]))
+        lcd.text(sys.argv[4])
+    if sys.argv[1] == 'backlight':
+        lcd.backlight(int(sys.argv[2]))
+    if sys.argv[1] == 'cls':
+        lcd.cls()
